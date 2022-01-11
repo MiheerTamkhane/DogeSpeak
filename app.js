c84f2478b436a4b1134c9806d92b67b3799c26b0
Miheer Tamkhane <54612063+MiheerTamkhane@users.noreply.github.com>
2022-01-11 19:41:17 +0530

    updated to ES6

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
-serverURL = "https://api.funtranslations.com/translate/doge.json";
+//url of api
+const serverURL = "https://api.funtranslations.com/translate/doge.json";
 
 const button = document.querySelector("button");
 const inputText = document.querySelector("#text-area");
 const outputDiv = document.querySelector(".output");
-const getURL = (text) => {
-  console.log(text);
-  return serverURL + "?text=" + text;
-};
 
+// getURL fuction which adds user input to url.
+const getURL = (text) => serverURL + "?text=" + text;
+
+// error fuction
 const errHandler = (err) => {
   alert("Something went wrong! " + err);
 };
 
+// fuction which retrives data from api
 const clickHandler = () => {
   let textInput = inputText.value;
-
   //contacting with api
   fetch(getURL(textInput))
     .then((res) => res.json())
-    .then((data) => {
-      outputDiv.innerText = data.contents.translated;
-    })
+    .then((data) => (outputDiv.innerText = data.contents.translated))
     .catch(errHandler);
 };
 
+// Event handlers
 button.addEventListener("click", clickHandler);
